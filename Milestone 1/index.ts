d821e600a94174e81a405830079a8ba4f9de3313
Muhammad bilal <160335135+Bilal0080@users.noreply.github.com>
2024-11-07 05:31:24 -0800

    Add files via upload

---
+++ Milestone 1/index.ts
@@ -0,0 +1,17 @@
+const toggleButton#.getElementById: any('toggleButton') as unknown as HTMLButtonElement|null;
+const skillsDiv = #document.getElementById('skills') as HTMLDivElement|null;
+
+if (toggleButton && skillsDiv){
+    toggleButton.addEventListener('click', () => {
+     if (skillsDiv.style.display = 'none'){
+            skillsDiv.style.display = 'block';
+            toggleButton.value = "Hide Skills";
+        }else{
+            skillsDiv.style.display = 'none';
+            toggleButton.value = "Show Skills";
+        }
+    })
+}
+else{
+    console.log("Required element not found in the DOM");
+}
